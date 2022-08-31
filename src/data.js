049f6e5ac79078e0988a54e6c9bc56f110517c77
Sstanley45 <stanleynjuguna445@gmail.com>
2022-08-31 11:46:03 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id : 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists. is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia",
+        imageUrl: "https://media.istockphoto.com/photos/japanese-landscape-at-sunset-picture-id1404688193?b=1&k=20&m=1404688193&s=170667a&w=0&h=5AiHZLKM5i77CmcHP7Cm-wdwBMgZMbepZ9oh-vjMVTw="
+    },
+    {
+        id : 2,
+        title: "Sydney Opera House",
+        location: "Brazil",
+        
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia",
+        imageUrl: "https://media.istockphoto.com/photos/boat-wakes-in-sydney-harbour-with-opera-house-and-bridge-in-picture-id1372050536?b=1&k=20&m=1372050536&s=170667a&w=0&h=iB-kgqDLGla3A2-jSj0BXINEixfHGjQ63dkJy5fTrJg="
+    },
+    {
+        id : 3,
+        title: "Gabra Tula",
+        location: "Kenya",
+        
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Gabra Tula is the highest mountain in Kenya and the second-highest in Africa, after Kilimanjaro. The highest peaks of the mountain are Batian, Nelion and Point Lenana. Wikipedia is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia",
+        imageUrl: "https://media.istockphoto.com/photos/japanese-landscape-at-sunset-picture-id1404688193?b=1&k=20&m=1404688193&s=170667a&w=0&h=5AiHZLKM5i77CmcHP7Cm-wdwBMgZMbepZ9oh-vjMVTw="
+    },
+    {
+        id : 2,
+        title: "Sydney Opera House",
+        location: "Brazil",
+        
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture. Wikipedia",
+        imageUrl: "https://media.istockphoto.com/photos/boat-wakes-in-sydney-harbour-with-opera-house-and-bridge-in-picture-id1372050536?b=1&k=20&m=1372050536&s=170667a&w=0&h=iB-kgqDLGla3A2-jSj0BXINEixfHGjQ63dkJy5fTrJg="
+    }
+]
